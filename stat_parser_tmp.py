d4b5dee626edc00498f819e91baa9f7c8a897373
Greg Starek <gregwsta@gmail.com>
2015-11-20 13:16:38 -0600

    tmp files cleaned

--- stat_parser_tmp.py
+++ stat_parser_tmp.py
@@ -41,50 +41,6 @@ print teams_line()
 """
 
 
-
-
-"""
-class Team(stat):
-
-	def __init__(self, TeamName, Year, Week):
-	self.TeamName = TeamName
-	self.Year = Year
-
-
-	def total_yards_line():
-		line_no=int(commands.getoutput("grep -n '<br />Total Yards</td>' tmp/2000-08-27-penn-state_soup.html | awk -F':' '{print $1}'"))
-		return line_no
-		
-	def passing_line():
-		line_no=int(commands.getoutput("grep -n '<br />Passing</td>' tmp/2000-08-27-penn-state_soup.html | awk -F':' '{print $1}'"))
-		return line_no
-		
-	def rushing_line():
-		line_no=int(commands.getoutput("grep -n '<br />Rushing</td>' tmp/2000-08-27-penn-state_soup.html | awk -F':' '{print $1}'"))
-		return line_no
-		
-	def first_downs_line():
-		line_no=int(commands.getoutput("grep -n '<br />First Downs</td>' tmp/2000-08-27-penn-state_soup.html | awk -F':' '{print $1}'"))
-		return line_no
-		
-	def penalties_line():
-		line_no=int(commands.getoutput("grep -n '<br />Penalties</td>' tmp/2000-08-27-penn-state_soup.html | awk -F':' '{print $1}'"))
-		return line_no
-		
-	def turnovers_line():
-		line_no=int(commands.getoutput("grep -n '<br />Turnovers</td>' tmp/2000-08-27-penn-state_soup.html | awk -F':' '{print $1}'"))
-		return line_no
-		
-	def teams_line():
-		line_no=int(commands.getoutput("grep -n '<table id=\"schools\" class=\"stats_table\">' tmp/2000-08-27-penn-state_soup.html | awk -F':' '{print $1}'"))
-		return line_no
-
-"""
-
-
-
-
-
 #stats=["total_yards_line()", "passing_line()", "rushing_line", "first_downs_line()", "penalties_line()", "turnovers_line()", "teams_line"]
 
 #Use line numbers to get values
