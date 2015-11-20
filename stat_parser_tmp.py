59d5c89e161340a4e5cd54f549c7f7f5fda93337
Greg Starek <gregwsta@gmail.com>
2015-11-20 14:48:10 -0600

    Cleaned stat parser

--- stat_parser_tmp.py
+++ stat_parser_tmp.py
@@ -1,94 +1,39 @@
 import commands
 
+stats=["Total Yards", "Passing", "Rushing", "First Downs", "Penalties", "Turnovers"]
 
-#Use flags to find relevant stat info in file
-def total_yards_line():
-	line_no=int(commands.getoutput("grep -n '<br />Total Yards</td>' tmp/2000-08-27-penn-state_soup.html | awk -F':' '{print $1}'"))
-	return line_no
-	
-def passing_line():
-	line_no=int(commands.getoutput("grep -n '<br />Passing</td>' tmp/2000-08-27-penn-state_soup.html | awk -F':' '{print $1}'"))
-	return line_no
-	
-def rushing_line():
-	line_no=int(commands.getoutput("grep -n '<br />Rushing</td>' tmp/2000-08-27-penn-state_soup.html | awk -F':' '{print $1}'"))
-	return line_no
-	
-def first_downs_line():
-	line_no=int(commands.getoutput("grep -n '<br />First Downs</td>' tmp/2000-08-27-penn-state_soup.html | awk -F':' '{print $1}'"))
-	return line_no
-	
-def penalties_line():
-	line_no=int(commands.getoutput("grep -n '<br />Penalties</td>' tmp/2000-08-27-penn-state_soup.html | awk -F':' '{print $1}'"))
-	return line_no
-	
-def turnovers_line():
-	line_no=int(commands.getoutput("grep -n '<br />Turnovers</td>' tmp/2000-08-27-penn-state_soup.html | awk -F':' '{print $1}'"))
-	return line_no
-	
-def teams_line():
+def get_away_team():
 	line_no=int(commands.getoutput("grep -n '<table id=\"schools\" class=\"stats_table\">' tmp/2000-08-27-penn-state_soup.html | awk -F':' '{print $1}'"))
+	away_team=commands.getoutput(''.join("awk 'NR==" + str(line_no+1) + "' tmp/2000-08-27-penn-state_soup.html")).split("</th>")[1].strip("<th>")
+	return away_team
+	
+def get_home_team():
+	line_no=int(commands.getoutput("grep -n '<table id=\"schools\" class=\"stats_table\">' tmp/2000-08-27-penn-state_soup.html | awk -F':' '{print $1}'"))
+	home_team=commands.getoutput(''.join("awk 'NR==" + str(line_no+1) + "' tmp/2000-08-27-penn-state_soup.html")).split("</th>")[2].strip("<th>")
+	return home_team
+
+def get_line(stat):
+	line_no=int(commands.getoutput(''.join("grep -n '<br />" + stat + "</td>' tmp/2000-08-27-penn-state_soup.html | awk -F':' '{print $1}'")))
 	return line_no
+	
+def away_stat(stat):
+	stat_line=get_line(stat) + 2
+	stat_value=commands.getoutput(''.join("awk 'NR==" + str(stat_line) + "' tmp/2000-08-27-penn-state_soup.html")).split(">")[1].strip("</th></div>")
+	return stat_value
 
-"""
-print total_yards_line()
-print passing_line()
-print rushing_line()
-print first_downs_line()
-print penalties_line()
-print turnovers_line()
-print teams_line()
-"""
-
-
-#stats=["total_yards_line()", "passing_line()", "rushing_line", "first_downs_line()", "penalties_line()", "turnovers_line()", "teams_line"]
-
-#Use line numbers to get values
-
-away_yards=commands.getoutput(''.join("awk 'NR==" + str(total_yards_line()+2) + "' tmp/2000-08-27-penn-state_soup.html")).split(">")[1].strip("</th></div>")
-print away_yards
-
-home_yards=commands.getoutput(''.join("awk 'NR==" + str(total_yards_line()+4) + "' tmp/2000-08-27-penn-state_soup.html")).split(">")[1].strip("</th></div>")
-print home_yards
-
-away_passing=commands.getoutput(''.join("awk 'NR==" + str(passing_line()+2) + "' tmp/2000-08-27-penn-state_soup.html")).split(">")[1].strip("</td>")
-print away_passing
-
-home_passing=commands.getoutput(''.join("awk 'NR==" + str(passing_line()+4) + "' tmp/2000-08-27-penn-state_soup.html")).split(">")[1].strip("</td>")
-print home_passing
-
-away_rushing=commands.getoutput(''.join("awk 'NR==" + str(rushing_line()+2) + "' tmp/2000-08-27-penn-state_soup.html")).split(">")[1].strip("</td>")
-print away_rushing
-
-home_rushing=commands.getoutput(''.join("awk 'NR==" + str(rushing_line()+4) + "' tmp/2000-08-27-penn-state_soup.html")).split(">")[1].strip("</td>")
-print home_rushing
-
-away_first_downs=commands.getoutput(''.join("awk 'NR==" + str(first_downs_line()+2) + "' tmp/2000-08-27-penn-state_soup.html")).split(">")[1].strip("</td>")
-print away_first_downs
-
-home_first_downs=commands.getoutput(''.join("awk 'NR==" + str(first_downs_line()+4) + "' tmp/2000-08-27-penn-state_soup.html")).split(">")[1].strip("</td>")
-print home_first_downs
-
-away_penalties=commands.getoutput(''.join("awk 'NR==" + str(penalties_line()+2) + "' tmp/2000-08-27-penn-state_soup.html")).split(">")[1].strip("</td>")
-print away_penalties
-
-home_penalties=commands.getoutput(''.join("awk 'NR==" + str(penalties_line()+4) + "' tmp/2000-08-27-penn-state_soup.html")).split(">")[1].strip("</td>")
-print home_penalties
-
-away_turnovers=commands.getoutput(''.join("awk 'NR==" + str(turnovers_line()+2) + "' tmp/2000-08-27-penn-state_soup.html")).split(">")[1].strip("</td>")
-print away_turnovers
-
-home_turnovers=commands.getoutput(''.join("awk 'NR==" + str(turnovers_line()+4) + "' tmp/2000-08-27-penn-state_soup.html")).split(">")[1].strip("</td>")
-print home_turnovers
+def home_stat(stat):
+	stat_line=get_line(stat) + 4
+	stat_value=commands.getoutput(''.join("awk 'NR==" + str(stat_line) + "' tmp/2000-08-27-penn-state_soup.html")).split(">")[1].strip("</th></div>")
+	return stat_value
 
 
-away_team=commands.getoutput(''.join("awk 'NR==" + str(teams_line()+1) + "' tmp/2000-08-27-penn-state_soup.html")).split("</th>")[1].strip("<th>")
-home_team=commands.getoutput(''.join("awk 'NR==" + str(teams_line()+1) + "' tmp/2000-08-27-penn-state_soup.html")).split("</th>")[2].strip("<th>")
+print get_home_team()
+for stat in stats:
+	print stat, home_stat(stat)
+	
+
+print get_away_team()
+for stat in stats:
+	print stat, away_stat(stat)
 
 
-
-
-
-print away_team
-print home_team
-print home_first_downs
