fe86940924284cd92762a6ac538559fb5f780b91
Greg Starek <gregwsta@gmail.com>
2015-11-27 09:39:37 -0600

    Database compiler adds column headers

--- compile_database.py
+++ compile_database.py
@@ -1,7 +1,7 @@
 import datetime
 import glob
+import commands
 
-teamlist=[]
 years=["2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014"]
 
 def get_week(filename):
@@ -10,25 +10,24 @@ def get_week(filename):
 	return datetime.datetime(int(year), month, day).weekday()
 
 for year in years:
+	print "Compiling weekly database for", ''.join(year + "...")
 	week = 1
 	counter = 0	
 	for filename in glob.glob(year + "/*_soup.html"):
-		print filename
 		if int(get_week(filename)) < int(counter):
 			week += 1
 		counter = int(get_week(filename))
-		print week
-	
-	
+
 	#Template file for database: preseason/week 0 (lists all 1A teams)
 	with open(''.join(year + "/conferences_" + year + ".csv"), "r") as filename:
 		for line in filename.readlines():
 			team=line.split(",")[0]
 			conference=line.split(",")[1]
 			for n in range(0, week+1):
+				curr_file=''.join(year + "/week_" + str(n) + "_" + year + ".csv")
 				template_file=open(''.join(year + "/week_" + str(n) + "_" + year + ".csv"), "a")
-				template_file.write(''.join(line.strip() + ",\n"))
+				template_file.write(''.join(line.strip() + ",,,,,,,\n"))
 
-
-
-
+#Pop in header
+for curr_file in glob.glob("*/week*.csv"):
+	commands.getoutput("echo 'Team,Conference,TotalYards,Passing,Rushing,FirstDowns,Penalties,Turnovers' | cat - " + curr_file + " | sponge " + curr_file)
